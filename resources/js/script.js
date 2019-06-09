b4805831a03d7cc5be66d30e5acbedbc774a4beb
Paweł Łukasiewicz <aq12aq12@gmail.com>
2019-06-09 14:33:11 +0200

    Performance optimalization of images
    Compressed optimalization main and minnor images for better performance :). It will be the "speediest" website ;)...soon.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,17 +1,5 @@
 
 
-/* Topnav example */
-
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
 function myFunction2() {
     var x = document.getElementById("js--main-nav");
     if (x.className === "main-nav") {

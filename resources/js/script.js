863742acab2284298a13814c08b160e59719b89d
Paweł Łukasiewicz <aq12aq12@gmail.com>
2019-07-16 16:44:36 +0200

    Added first subpage and some corrections
    1. I've added first subpage, where i will put little to-do list.
    2. I corrected some containers and changed grid into flexbox - yes, it's good tool :)

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,4 @@
 
-<<<<<<< Updated upstream
-=======
 /*Sticky nav */
 window.onscroll = function() {myFunction()};
 
@@ -8,30 +6,40 @@ window.onscroll = function() {myFunction()};
 function myFunction() {
     var navbar = document.getElementById("navbar");
     var sticky = navbar.offsetTop;
-    console.log(sticky);
-
     if (window.pageYOffset > sticky && window.pageYOffset> 670) {
-        navbar.classList.add("sticky")
-        console.log(sticky);
+        navbar.classList.add("sticky");
     } else {
         navbar.classList.remove("sticky");
+
     }
 }
 
->>>>>>> Stashed changes
 
 function myFunction2() {
     var x = document.getElementById("js--main-nav");
     if (x.className === "main-nav") {
         x.className += " responsive";
+       
     } else {
         x.className = "main-nav";
     }
 }
-<<<<<<< Updated upstream
-=======
+
+/* Change content in offers */
+window.onresize = function () {
+    if (window.innerWidth <= 650) {
+        document.getElementById("bushcraft").innerHTML = "<h3>Bushcrafting</h3>";
+        document.getElementById("climb").innerHTML = "<h3>Incredible Climbing!</h3>";
+        document.getElementById("fire").innerHTML = "<h3> lighting a fire!</h3>";
+        document.getElementById("beach").innerHTML = "<h3>Walking on beach!</h3>";
+    } else if (window.innerWidth > 650) {
+        document.getElementById("bushcraft").innerHTML = "<h3>Bushcrafting</h3>-We will be fining the best forests in your life<br>-You will be having the best adventure <br>-We will survive...and many many more<br>";
+        document.getElementById("climb").innerHTML = "<h3>Incredible Climbing!</h3>-We will be climbing in the beautiful mountains<br> -You will be training your muscles every day<br>-You will be stronger and more agility <br>";
+        document.getElementById("fire").innerHTML = "<h3> lighting a fire!</h3>-We Light a fire on evening<br>-We are using only real wood<br>-You will find the best pleaces for bonfire<br>";
+        document.getElementById("beach").innerHTML = "<h3>Walking on beach!</h3>-We will be walking everyday<br>-You will be walking across the beach from 5 a.m to 7 a.m<br>-You will have to walking not less than 45 minutes<br>";
+    }
+}
 
 
 /*Scroll on buttons*/
 
->>>>>>> Stashed changes

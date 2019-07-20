68632277c3aaa337883e804e50c08e2e0d16ff9a
Paweł Łukasiewicz <aq12aq12@gmail.com>
2019-07-20 20:10:10 +0200

    Added animations
    Hey! I've finished fun with animations...Main page has more 'life' :) .

--- resources/js/script.js
+++ resources/js/script.js
@@ -41,5 +41,22 @@ window.onresize = function () {
 }
 
 
-/*Scroll on buttons*/
+/*animations on scroll*/
+
+  $('.js--wp-1').waypoint(function(direction) {
+        
+        $('.js--wp-1').addClass('animated fadeInUp');
+    }, {
+    
+    offset: '50%'        
+    });
+$('.js--wp-2').waypoint(function(direction) {
+        
+        $('.js--wp-2').addClass('animated fadeIn');
+    }, {
+    
+    offset: '50%'        
+    });
+
+
 
